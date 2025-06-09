5109c4691d7d5da0ed02ac641e6382d2a39588cc
ballsten <andrew@theaker.name>
2025-06-09 15:11:54 +1000

    feat: ts-comments

---
+++ nvim/plugin/ts-comments.lua
@@ -0,0 +1,12 @@
+-- only load once
+if vim.g.loaded_plugin_tscomments then
+  return
+end
+vim.g.loaded_plugin_tscomments = true
+
+-- setup
+require('ts-comments').setup({
+  lang = {
+    nix = "# %s"
+  }
+})
