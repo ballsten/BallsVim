fcaebe09c2260aac8d04bc8ad6f8bd492e21076f
ballsten <andrew@theaker.name>
2025-06-09 15:15:30 +1000

    feat: lazydev

---
+++ nvim/plugin/lazydev.lua
@@ -0,0 +1,14 @@
+-- only load once
+if vim.g.loaded_plugin_lazydev then
+  return
+end
+vim.g.loaded_plugin_lazydev = true
+
+require("lazydev").setup({
+  library = {
+      { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+      { path = "snacks.nvim", words = { "Snacks" } },
+    }
+})
+
+
