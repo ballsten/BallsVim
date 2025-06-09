50201a0d23eb02afd9251a2592072c02f9891da8
ballsten <andrew@theaker.name>
2025-06-09 15:03:55 +1000

    feat: mini-ai

---
+++ nvim/plugin/mini.lua
@@ -0,0 +1,68 @@
+-- only load once
+if vim.g.loaded_plugin_mini then
+  return
+end
+vim.g.loaded_plugin_mini = true
+
+require('mini.icons').setup {
+  file = {
+    ['.keep'] = { glyph = '󰊢', hl = 'MiniIconsGrey' },
+    ['devcontainer.json'] = { glyph = '', hl = 'MiniIconsAzure' },
+  },
+  filetype = {
+    dotenv = { glyph = '', hl = 'MiniIconsYellow' },
+  },
+}
+
+require('mini.pairs').setup {
+  modes = { insert = true, command = true, terminal = false },
+  -- skip autopair when next character is one of these
+  skip_next = [=[[%w%%%'%[%"%.%`%$]]=],
+  -- skip autopair when the cursor is inside these treesitter nodes
+  skip_ts = { 'string' },
+  -- skip autopair when next character is closing pair
+  -- and there are more closing pairs than opening pairs
+  skip_unbalanced = true,
+  -- better deal with markdown code blocks
+  markdown = true,
+}
+
+local ai = require('mini.ai')
+
+-- taken from MiniExtra.gen_ai_spec.buffer
+local function ai_buffer(ai_type)
+  local start_line, end_line = 1, vim.fn.line('$')
+  if ai_type == 'i' then
+    -- Skip first and last blank lines for `i` textobject
+    local first_nonblank, last_nonblank = vim.fn.nextnonblank(start_line), vim.fn.prevnonblank(end_line)
+    -- Do nothing for buffer with all blanks
+    if first_nonblank == 0 or last_nonblank == 0 then
+      return { from = { line = start_line, col = 1 } }
+    end
+    start_line, end_line = first_nonblank, last_nonblank
+  end
+
+  local to_col = math.max(vim.fn.getline(end_line):len(), 1)
+  return { from = { line = start_line, col = 1 }, to = { line = end_line, col = to_col } }
+end
+
+require('mini.ai').setup {
+  n_lines = 500,
+  custom_textobjects = {
+    o = ai.gen_spec.treesitter { -- code block
+      a = { '@block.outer', '@conditional.outer', '@loop.outer' },
+      i = { '@block.inner', '@conditional.inner', '@loop.inner' },
+    },
+    f = ai.gen_spec.treesitter { a = '@function.outer', i = '@function.inner' }, -- function
+    c = ai.gen_spec.treesitter { a = '@class.outer', i = '@class.inner' }, -- class
+    t = { '<([%p%w]-)%f[^<%w][^<>]->.-</%1>', '^<.->().*()</[^/]->$' }, -- tags
+    d = { '%f[%d]%d+' }, -- digits
+    e = { -- Word with case
+      { '%u[%l%d]+%f[^%l%d]', '%f[%S][%l%d]+%f[^%l%d]', '%f[%P][%l%d]+%f[^%l%d]', '^[%l%d]+%f[^%l%d]' },
+      '^().*()$',
+    },
+    g = ai_buffer, -- buffer
+    u = ai.gen_spec.function_call(), -- u for "Usage"
+    U = ai.gen_spec.function_call { name_pattern = '[%w_]' }, -- without dot in function name
+  },
+}
